378d99888b80529af9248c74dcc78e2013963abc
Stephen Kel <s.kel@student.qut.edu.au>
2019-10-31 20:22:55 +1000

    code refactor

--- src/components/pages/CoderProfilePage.tsx
+++ src/components/pages/CoderProfilePage.tsx
@@ -9,7 +9,7 @@ type GUID = string;
 interface IRouteParams extends RouteComponentProps { id: GUID }
 
 const requestBody = {
-    query: `		
+	query: `		
 	query pgm($uuid: ID!) 
 		{
 			programmer(id: $uuid) 
@@ -23,74 +23,57 @@ const requestBody = {
 					lastName
 					avatar
 					bio
+					projects { id name }
+					skills {id name	}
+					works {	title startDate endDate }
+					qualifications { id title }
 				}
-				country { name	}
-				state{
-					id
-					name
-					postCode
-				}
-				projects {
-					id
-					name
-				}
-				skills {
-					id
-					name
-				}
-				works {
-					title
-					startDate
-					endDate
-				}
-				qualifications {
-					id
-					title
-				}
+				country { name }
+				state{ id name postCode }	
 			}
 		}`
 };
 
 const CoderProfilePage: React.FunctionComponent<RouteComponentProps> = ({ match }) => {
-    const { id } = match.params as IRouteParams;
-    const store = useContext(CodersStore);
-    const { loadingInitial, programmer, loadProgrammer } = store;
+	const { id } = match.params as IRouteParams;
+	const store = useContext(CodersStore);
+	const { loadingInitial, programmer, loadProgrammer } = store;
 
-    //const [programmer, setData] = useState<Programmer>();
+	//const [programmer, setData] = useState<Programmer>();
 
-    useEffect(() => {
-        //getGraphQlServerResult(requestBody.query, {uuid: id});	
-        loadProgrammer(requestBody.query, { uuid: id });
-    }, [loadProgrammer, id]);
+	useEffect(() => {
+		//getGraphQlServerResult(requestBody.query, {uuid: id});	
+		loadProgrammer(requestBody.query, { uuid: id });
+	}, [loadProgrammer, id]);
 
-    if (programmer === undefined || loadingInitial)
-        return <div className="container-fluid"><h3>Loading ....</h3></div>;
+	if (programmer === undefined || loadingInitial)
+		return <div className="container-fluid"><h3>Loading ....</h3></div>;
 
-    const { profile: { avatar, firstName, lastName, bio } } = programmer;
-    return (
-        <div className="container card-transition" style={{ overflowY: "hidden" }}>
-            <section className="d-flex flex-row justify-content-center">
-                <div className="card my-3" style={{ maxWidth: "100vh", minHeight: "82vh", overflowY: "hidden" }}>
-                    <div className="row no-gutters">
-                        <div className="col-md-4">
-                            <img className="card-img" src={avatar || "http://localhost:3000/assets/coder-default.png"} alt="coder" />
-                        </div>
-                        <div className="col-md-8">
-                            <div className="card-body">
-                                <h5 className="card-title">{getFullName(firstName, lastName)}</h5>
-                                <h6 className="card-subtitle mb-2 text-muted">Senior Developer</h6>
-                                <p className="card-text">{bio}.</p>
-                                <h6 className="card-subtitle mb-2 text-muted">Skills</h6>
-                                <div></div>
-                                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </div>
+	const { profile: { avatar, firstName, lastName, bio } } = programmer;
+	return (
+		<div className="container card-transition" style={{ overflowY: "hidden" }}>
+			<section className="d-flex flex-row justify-content-center">
+				<div className="card my-3" style={{ maxWidth: "100vh", minHeight: "82vh", overflowY: "hidden" }}>
+					<div className="row no-gutters">
+						<div className="col-md-4">
+							<img className="card-img" src={avatar || "http://localhost:3000/assets/coder-default.png"} alt="coder" />
+						</div>
+						<div className="col-md-8">
+							<div className="card-body">
+								<h5 className="card-title">{getFullName(firstName, lastName)}</h5>
+								<h6 className="card-subtitle mb-2 text-muted">Senior Developer</h6>
+								<p className="card-text">{bio}.</p>
+								<h6 className="card-subtitle mb-2 text-muted">Skills</h6>
+								<div></div>
+								<p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
 
-    )
+	)
 }
 
 // Export home page component
